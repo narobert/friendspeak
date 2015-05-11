5cec1b885cd961db84eb093a844b161372ea8d8b
Nick Roberts <narobert@unknowne4ce8f2f30da.attlocal.net>
2015-05-11 13:31:20 -0700

    trying to access userid in friendlist

--- static/js/fbAuthLogin.js
+++ static/js/fbAuthLogin.js
@@ -110,6 +110,7 @@ console.log('userInformation userID sent:'+userID);
                 var profLocale = response.locale;
                 var profAge;
                 var profPicture = "https://graph.facebook.com/" + userID + "/picture?width=200";
+                console.log(response.id);
                 
                 function agefinding()
                      {
