54144af445f2fe3522f21e58330686f697d2bfbd
Nick Roberts <narobert@unknowne4ce8f2f30da.attlocal.net>
2015-05-12 14:57:36 -0700

    comparing images did not work

--- static/js/fbDataRetrieval.js
+++ static/js/fbDataRetrieval.js
@@ -77,45 +77,6 @@
                   console.log('fbDataRetrieval profileName:'+name);
                   //document.getElementById("profileName");
                   //document.getElementById["profileUserID"].innerHTML= name;
-
-
-
-                  var a = new Image();
-                  var b = new Image();
-                  a.src = response.picture.data.url;
-                  b.src = 'https://graph.facebook.com/'+response.id+'/picture?width=200';
-                  var a_base64 = getBase64Image(a);
-                  var b_base64 = getBase64Image(b);
-                  if (a_base64 === b_base64) {
-                      console.log("success!");
-                  }
-                  else {
-                      console.log("fail :(");
-                  }
-
-function getBase64Image(img) {
-    // Create an empty canvas element
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-
-    // Copy the image contents to the canvas
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-
-    // Get the data-URL formatted image
-    // Firefox supports PNG and JPEG. You could check img.src to
-    // guess the original format, but be aware the using "image/jpg"
-    // will re-encode the image.
-    var dataURL = canvas.toDataURL("image/png");
-
-    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-}
-
-
-
-
-
                   
                   //taggable_friends for loop
                   var friendsIDnames = [];
