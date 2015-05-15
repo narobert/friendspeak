14c3f6fd5c478fe007752ddcdeee36767ad9668b
Nick Roberts <narobert@unknowne4ce8f2f30da.attlocal.net>
2015-05-15 15:05:32 -0700

    cleaning up code

--- static/js/fbAuthLogin.js
+++ static/js/fbAuthLogin.js
@@ -1,129 +1,130 @@
- // called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
+// called with the results from from FB.getLoginStatus().
+function statusChangeCallback(response) {
     console.log('accessToken & status:'+JSON.stringify(response));
     
     var sendResponse = response;
     console.log('statusChangeCallback sendResponse:'+sendResponse);
-     if (response.status === 'connected') {
-     var userID = sendResponse.authResponse.userID;
-      userInformation(userID);
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      console.log('response.status==not_authorized: logged into fb but not app; logging out of fb now');
-      document.getElementById('status').innerHTML = 'Please log in to facebook again ';
-    } else {
-      console.log('user is not logged into facebook, trying new login rerequest');
-      // person not logged into Facebook; unsure if they're logged into app or not.
-      // document.getElementById('status').innerHTML = 'Please log into Facebook.';
-      FB.login(function(response) {
-        console.log('user logged out rerequest');
-      },{scope: 'user_birthday,user_about_me,user_location,user_friends',
-        auth_type: 'rerequest'
-     }); 
+    if (response.status === 'connected') {
+        var userID = sendResponse.authResponse.userID;
+        userInformation(userID);
+    } 
+    else if (response.status === 'not_authorized') {
+        // The person is logged into Facebook, but not your app.
+        console.log('response.status==not_authorized: logged into fb but not app; logging out of fb now');
+        document.getElementById('status').innerHTML = 'Please log in to facebook again ';
     }
-  }
+    else {
+        console.log('user is not logged into facebook, trying new login rerequest');
+        // person not logged into Facebook; unsure if they're logged into app or not.
+        // document.getElementById('status').innerHTML = 'Please log into Facebook.';
+        FB.login(function(response) {
+            console.log('user logged out rerequest');
+        }, {scope: 'user_birthday,user_about_me,user_location,user_friends',
+            auth_type: 'rerequest'
+        }); 
+    }
+} 
   
-  
-  
-  function checkLoginState() {
+function checkLoginState() {
     FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
+        statusChangeCallback(response);
     });
-  }
+}
 
 window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '1565760477011269',
-    cookie     : true,  
-    xfbml      : true,  
-    version    : 'v2.3' 
-  });
-  FB.Event.subscribe('auth.login', function (response) {
-      window.location = "https://friendspeak.herokuapp.com";
- });
+    FB.init({
+        appId      : '1565760477011269',
+        cookie     : true,  
+        xfbml      : true,  
+        version    : 'v2.3' 
+    });
+    FB.Event.subscribe('auth.login', function (response) {
+        window.location = "https://friendspeak.herokuapp.com";
+    });
 
-  FB.getLoginStatus(function(response) {
-          statusChangeCallback(response);
-  });
-  
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });  
 };
-  //load the SDK asynchronously
-   (function(d, s, id) {
+
+//load the SDK asynchronously
+(function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
+    js = d.createElement(s);
+    js.id = id;
     js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=1565760477011269";
     fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk')); 
+}(document, 'script', 'facebook-jssdk')); 
 
-function newLoginDialog(){
-console.log('IN NEWLOGINDIALOG');
+function newLoginDialog() {
+    console.log('IN NEWLOGINDIALOG');
     FB.login(function(response) {
-    console.log('granted scopes:'+authResponse.grantedScopes);
+        console.log('granted scopes:'+authResponse.grantedScopes);
 
-    },{scope: 'user_birthday,user_about_me,user_location,user_friends',
+    }, {scope: 'user_birthday,user_about_me,user_location,user_friends',
         auth_type: 'rerequest',
         return_scopes: true
-   });
+    });
 }
-function userInformation(userID){
-      var userLocale = "en_US";
-      var userDisplayName = "";
-      var permissionDeclinedCount;
-      console.log('userInformation userID sent:'+userID+' making fb.api call.');
 
-      FB.api(
+function userInformation(userID) {
+    var userLocale = "en_US";
+    var userDisplayName = "";
+    var permissionDeclinedCount;
+    console.log('userInformation userID sent:'+userID+' making fb.api call.');
+
+    FB.api(
         '/me?fields=permissions,name,locale,birthday,picture,email,bio',
         function (response) {
-          //console.log('userInformation response in fb.api call:'+JSON.stringify(response));
-          if (response && !response.error) {
-               permissionDeclinedCount = 0;
+            //console.log('userInformation response in fb.api call:'+JSON.stringify(response));
+            if (response && !response.error) {
+                permissionDeclinedCount = 0;
                 var permissionsArray = response.permissions.data;
                 //console.log('full response'+JSON.stringify(response));
                 //console.log('response.permissions: '+JSON.stringify(response.permissions));
                 console.log('permissionsArray.count:'+permissionsArray.count);
                 
-
                 function inPermissions(arr) {
-                  for(var i=0; i<arr.length; i++) {
-                      if (arr[i]["status"] == "declined"){
-                        permissionDeclinedCount++; 
-                      }
-                      console.log('inPermissions(arr) for loop');
-                  }
-                  if(permissionDeclinedCount >= 1){
-                    console.log('permissionCount >= 1, they denied:'+permissionDeclinedCount+' permissions');
+                    for (var i=0; i<arr.length; i++) {
+                        if (arr[i]["status"] == "declined") {
+                            permissionDeclinedCount++; 
+                        }
+                        console.log('inPermissions(arr) for loop');
+                    }
+                    if (permissionDeclinedCount >= 1) {
+                        console.log('permissionCount >= 1, they denied:'+permissionDeclinedCount+' permissions');
                     
-                    window.alert("Hi "+userDisplayName+", unfortunately we cannot log you in without gaining a little information about you so your friends know who their posting/talking to. "+
+                        window.alert("Hi "+userDisplayName+", unfortunately we cannot log you in without gaining a little information about you so your friends know who their posting/talking to. "+
                     "\n\nWe require your friend list (to display a list of friends that you can post to), email address (for user idenitification in our database), birthday (to calculate your age [actual month/day is not displayed]), current city (so your friends know where you are), and personal description (as a template to start with that you can later change)"+
                     "\n\nWe will now log you out; when you log back in you will be prompted for permissions and we hope you understand why this information is necessary!");
                     
-                    // window.location = "https://friendspeak.herokuapp.com/logout/";
-                    FB.logout(function(response) {
-                      // user is now logged out
-                      //FB.logout will log the user out of both your site and Facebook. You will need to have a valid access token for the user in order to call the function. (check)
-                      //Calling FB.logout will also invalidate the access token that you have for the user, unless you have extended the access token. More info on how to extend the access token here. (maybe need to convert to extended?)
-                      console.log('LOGGED OUT USER');
-                    });
-                    newLoginDialog(); //never runs
+                        // window.location = "https://friendspeak.herokuapp.com/logout/";
+                        FB.logout(function(response) {
+                            // user is now logged out
+                            //FB.logout will log the user out of both your site and Facebook. You will need to have a valid access token for the user in order to call the function. (check)
+                            //Calling FB.logout will also invalidate the access token that you have for the user, unless you have extended the access token. More info on how to extend the access token here. (maybe need to convert to extended?)
+                            console.log('LOGGED OUT USER');
+                        });
+                        newLoginDialog(); //never runs
 
-                  }else{
-                    console.log('all permissions granted');
-                  }
-                  if(response.status == 'connected'){
-                    window.location = "https://friendspeak.herokuapp.com";
-                  }
+                    }
+                    else {
+                        console.log('all permissions granted');
+                    }
+                    if (response.status == 'connected') {
+                        window.location = "https://friendspeak.herokuapp.com";
+                    }
                 }  
                 inPermissions(permissionsArray);
                 
-              if(permissionDeclinedCount == 0 ){
-                var profName = response.name;
-                var profLocale = response.locale;
-                var profAge;
-                var profPicture = "https://graph.facebook.com/" + userID + "/picture?width=200";
+                if (permissionDeclinedCount == 0) {
+                    var profName = response.name;
+                    var profLocale = response.locale;
+                    var profAge;
+                    var profPicture = "https://graph.facebook.com/" + userID + "/picture?width=200";
                 
-                function agefinding()
-                     {
+                    function agefinding() {
                         var birthDay = response.birthday;
                         var DOB = new Date(birthDay);
                         var today = new Date();
@@ -131,32 +132,34 @@ function userInformation(userID){
                         ageCalc = Math.floor(ageCalc / (1000 * 60 * 60 * 24 * 365.25));
                         return ageCalc;
                     }                  
-                  if(response.birthday != 'undefined'){
-                    profAge = agefinding();
-                    console.log('age:'+profAge);
-                  }else{
-                    profAge = 'null';
-                    console.log('cant find age');
-                  }             
-                  //document.getElementById("profileBio").value = profBio;
-                  //document.getElementById("profileEmail").value = profEmail;
-                  //document.getElementById("profileLocation").value = profLocation;
-                  document.getElementById("profilePicture").value = profPicture;
-                  document.getElementById("profileName").value = profName;
-                  document.getElementById("profileLocale").value = profLocale;
-                  document.getElementById("profileAge").value = profAge;       
-                  document.getElementById("profileUserID").value = userID;         //userID
-                  document.getElementById("password").value = "12345";    //super secret password key
-                  console.log('found username and password elements');
-                  document.getElementById('loginForm').submit();
-                  console.log('login form just submitted');
-              }else{
-                  console.log('declined permissions count:'+permissionDeclinedCount);
-              }
-            }else{
-              console.log('error: permissions retrieval::'+JSON.stringify(response));
+                    if (response.birthday != 'undefined') {
+                        profAge = agefinding();
+                        console.log('age:'+profAge);
+                    }
+                    else {
+                        profAge = 'null';
+                        console.log('cant find age');
+                    }             
+                    //document.getElementById("profileBio").value = profBio;
+                    //document.getElementById("profileEmail").value = profEmail;
+                    //document.getElementById("profileLocation").value = profLocation;
+                    document.getElementById("profilePicture").value = profPicture;
+                    document.getElementById("profileName").value = profName;
+                    document.getElementById("profileLocale").value = profLocale;
+                    document.getElementById("profileAge").value = profAge;       
+                    document.getElementById("profileUserID").value = userID;         //userID
+                    document.getElementById("password").value = "12345";    //super secret password key
+                    console.log('found username and password elements');
+                    document.getElementById('loginForm').submit();
+                    console.log('login form just submitted');
+                }
+                else {
+                    console.log('declined permissions count:'+permissionDeclinedCount);
+                }
+            }
+            else {
+                console.log('error: permissions retrieval::'+JSON.stringify(response));
             }
         }
-      );
-
-    }
+    );
+}
--- static/js/fbDataRetrieval.js
+++ static/js/fbDataRetrieval.js
@@ -1,113 +1,121 @@
 
- window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '1565760477011269',
-    cookie     : true,  // enable cookies to allow the server to access the session BASE.HTML
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.3' 
-  });
-  FB.Event.subscribe('auth.login', function (response) {
-      window.location = "https://friendspeak.herokuapp.com";
-  });
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '1565760477011269',
+        cookie     : true,  // enable cookies to allow the server to access the session BASE.HTML
+        xfbml      : true,  // parse social plugins on this page
+        version    : 'v2.3' 
+    });
+    FB.Event.subscribe('auth.login', function (response) {
+        window.location = "https://friendspeak.herokuapp.com";
+    });
 
-  FB.getLoginStatus(function(response) {
-      if (response.status === 'connected') {
-          statusChangeCallback(response);
-      }else{
-        window.location = "https://friendspeak.herokuapp.com/logout/";
-      } 
-  });
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            statusChangeCallback(response);
+        } 
+        else {
+            window.location = "https://friendspeak.herokuapp.com/logout/";
+        } 
+    });
 
-  function statusChangeCallback(response) {
-    // defines current login status of the person
-    if (response.status === 'connected') {
+    function statusChangeCallback(response) {
+        // defines current login status of the person
+        if (response.status === 'connected') {
             //address:The person's address
             //location: The person's current location as entered by them on their profile. This field is not related to check-ins
-           //name,birthday,location,bio,address,locale,email,picture,taggable_friends,friends",
+            //name,birthday,location,bio,address,locale,email,picture,taggable_friends,friends",
             FB.api(
                 "/me?fields=location,bio,email,taggable_friends,friends,address",
                 function (response) {
-                  if (response && !response.error) {
-                   console.log('Successful login for: ' + response.name + ' Email: ' + response.email);
+                    if (response && !response.error) {
+                        console.log('Successful login for: ' + response.name + ' Email: ' + response.email);
     
-                  // Start: menu bar
-                  var myFriendsArray = [];
-                  var numberOfFriendsApp = response.friends.data.length;
-                  var friendsApp = response.friends.data;
-                  var profileFriendsApp = document.getElementById("profileFriendsApp");
-                  var myFriends = "";
+                        // Start: menu bar
+                        var myFriendsArray = [];
+                        var numberOfFriendsApp = response.friends.data.length;
+                        var friendsApp = response.friends.data;
+                        var profileFriendsApp = document.getElementById("profileFriendsApp");
+                        var myFriends = "";
 
-                  for (var i = 0; i < numberOfFriendsApp; i++) {
-                    myFriends += "<li><a href='/profile/" + friendsApp[i].id + "/'>" + friendsApp[i].name + "</a></li>";
-                    myFriendsArray.push(friendsApp[i].name);
-                  }
+                        for (var i = 0; i < numberOfFriendsApp; i++) {
+                            myFriends += "<li><a href='/profile/" + friendsApp[i].id + "/'>" + friendsApp[i].name + "</a></li>";
+                            myFriendsArray.push(friendsApp[i].name);
+                        }
 
-                  var numberOfTaggableFriends = response.taggable_friends.data.length;
-                  var data = response.taggable_friends.data;
-                  //console.log('fbDataRetrieval, taggable_friends.data.length:'+numberOfTaggableFriends+', and taggable_friends.data:'+JSON.stringify(data));
-                  var profileTaggableFriends = document.getElementById("profileTaggableFriends");
-                  var taggableFriends = "";
+                        var numberOfTaggableFriends = response.taggable_friends.data.length;
+                        var data = response.taggable_friends.data;
+                        //console.log('fbDataRetrieval, taggable_friends.data.length:'+numberOfTaggableFriends+', and taggable_friends.data:'+JSON.stringify(data));
+                        var profileTaggableFriends = document.getElementById("profileTaggableFriends");
+                        var taggableFriends = "";
 
-                  for (var i = 0; i < numberOfTaggableFriends; i++) {
-                    if (myFriendsArray.indexOf(data[i].name) > -1) {
-                    //take out of taggable friends
-                    } else {
-                      taggableFriends += "<li><p id='sendInviteButton' style='margin:0;'>" + data[i].name + "</p></li>";
+                        for (var i = 0; i < numberOfTaggableFriends; i++) {
+                            if (myFriendsArray.indexOf(data[i].name) > -1) {
+                                //take out of taggable friends
+                            } 
+                            else {
+                                taggableFriends += "<li><p id='sendInviteButton' style='margin:0;'>" + data[i].name + "</p></li>";
+                            }
+                        }
+
+                        $('#sendInviteButton').live('click', function() {
+                            FB.ui({
+                                app_id: '1565760477011269',
+                                method: 'send',
+                                link: 'https://friendspeak.herokuapp.com',
+                            });
+                        });
+
+                        profileFriendsApp.innerHTML = myFriends;
+                        profileTaggableFriends.innerHTML = taggableFriends;
+                        // End: menu bar
+
+                        if (response.location != 'undefined' && response.location != 'unknown') {
+                            var location = response.location;
+                        }
+                        else {
+                            var location = "unknown";
+                        }
+                        var profileLocation = document.getElementById("profileLocation");
+                        profileLocation.innerHTML = location;
+
                     }
-                  }
-
-                  $('#sendInviteButton').live('click', function() {
-                    FB.ui({
-                      app_id: '1565760477011269',
-                      method: 'send',
-                      link: 'https://friendspeak.herokuapp.com',
-                    });
-                  });
-
-                  profileFriendsApp.innerHTML = myFriends;
-                  profileTaggableFriends.innerHTML = taggableFriends;
-                  // End: menu bar
-
-                  if(response.location != 'undefined' && response.location != 'unknown'){
-                    var location = response.location;
-                  }else{
-                    var location = "unknown";
-                  }
-                  var profileLocation = document.getElementById("profileLocation");
-                  profileLocation.innerHTML = location;
-
-                }else{
-                  console.log('error retrieving some shit:::'+JSON.stringify(response.error));
+                    else {
+                        console.log('error retrieving some shit:::'+JSON.stringify(response.error));
+                    }
                 }
-            });
+            );
       
-     function getUserLocation() {
-         FB.api(
-                "/v2.0/me?fields=location",
-                function (response) {
-                  if (response && !response.error) {
-                   console.log('getUserLocation, location:'+JSON.stringify(response));
-                  }else{
-                    console.log('error thrown in getUserLocation:'+response.error);
-                  }
-                });
-      }
-    }else if (response.status === 'not_authorized') {
-      console.log('person is logged into FB but not app');
-      // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log into FriendSpeak.';
-    } else {
-      // The person is not logged into Facebook; unsure if logged into app or not.
-      console.log('fbDataRetrieval: person not logged into FB or app');
-          //FB.login(function(response) {
-            ////taggable_friends,user_about_me,user_birthday,user_location
-            //// handle the response {"authResponse":null,"status":"unknown"} when exit out of dialog box
-            //console.log('FB.login response handling: this was where strinfigy response happened');
-          //}, {scope: 'public_profile,user_birthday,user_about_me,user_location,user_friends', 
+            function getUserLocation() {
+                FB.api(
+                    "/v2.0/me?fields=location",
+                    function (response) {
+                        if (response && !response.error) {
+                            console.log('getUserLocation, location:'+JSON.stringify(response));
+                        }
+                        else {
+                            console.log('error thrown in getUserLocation:'+response.error);
+                        }
+                    }
+                );
+            }
+        }
+        else if (response.status === 'not_authorized') {
+            console.log('person is logged into FB but not app');
+            // The person is logged into Facebook, but not your app.
+            document.getElementById('status').innerHTML = 'Please log into FriendSpeak.';
+        }
+        else {
+            // The person is not logged into Facebook; unsure if logged into app or not.
+            console.log('fbDataRetrieval: person not logged into FB or app');
+            //FB.login(function(response) {
+                //taggable_friends,user_about_me,user_birthday,user_location
+                //handle the response {"authResponse":null,"status":"unknown"} when exit out of dialog box
+                //console.log('FB.login response handling: this was where strinfigy response happened');
+            //}, {scope: 'public_profile,user_birthday,user_about_me,user_location,user_friends', 
             //return_scopes: true
-            //}
-          //);
-      document.getElementById('status').innerHTML = 'Please log into Facebook.';
+            //});
+            document.getElementById('status').innerHTML = 'Please log into Facebook.';
+        }
     }
-  }
 };
