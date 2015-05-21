c31ef64ddbc15658b01a73729718b4903298f240
Nick Roberts <narobert@unknowne4ce8f2f30da.attlocal.net>
2015-05-20 17:46:28 -0700

    pushing changes

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,6 @@ urlpatterns = patterns(
     (r'getCommentsW$', 'getCommentsW'),
     (r'getCommentsP$', 'getCommentsP'),
     (r'getCommentW$', 'getCommentW'),
-    (r'getCommentP$', 'getCommentP'),
     (r'getLikeW$', 'getLikeW'),
     (r'getLikesW$', 'getLikesW'),
     (r'getDislikeW$', 'getDislikeW'),
